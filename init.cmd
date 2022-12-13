3f0a182abd427223cb5185d5515471d98acab718
aacco <taigafuse@gmail.com>
2022-12-13 14:13:43 +0900

    Specify module versions in init.cmd

--- init.cmd
+++ init.cmd
@@ -1,4 +1,4 @@
-pip install selenium
-pip install requests
-pip install bs4
-pip install yattagpip install selenium
+pip install selenium==4.1.3
+pip install requests==2.27.1
+pip install bs4==0.0.1
+pip install yattag==1.14.0
